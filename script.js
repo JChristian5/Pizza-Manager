d1a7d3df23934c4dc05b4db1369b78338144717c
Jeremy Christian <jeremy.l.christian@gmail.com>
2022-10-10 16:25:52 -0700

    Fix and add delete button to delete exact element in list.

--- script.js
+++ script.js
@@ -5,24 +5,35 @@ const submitToppings = document.querySelector('#submittoppings');
 
 const submitList = document.querySelector('#toppingslist');
 
+
+
+
 toppingsForm.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
 submitToppings.addEventListener('click', () => {
-    boxTopping = document.createElement('div');
+    const boxTopping = document.createElement('div');
     let name = toppingsName.value;
     let number = toppingsNumber.value;
     boxTopping.innerHTML = `${name}: ${number}`;
     submitList.appendChild(boxTopping);
+    toppingsName.value = ''; //Deletes text inside input box
+    toppingsNumber.value = ''; //Deletes text inside input box
 
     const editButton = document.createElement('button');
     editButton.classList.add('btn', 'btn-outline-primary');
     editButton.innerHTML = 'Edit';
     boxTopping.appendChild(editButton);
-
+    
     const deleteButton = document.createElement('button');
     deleteButton.classList.add('btn', 'btn-outline-danger');
     deleteButton.innerHTML = 'Delete';
-    boxTopping.appendChild(deleteButton);
-})
+    boxTopping.appendChild(deleteButton); 
+
+    deleteButton.addEventListener('click', () => {
+        submitList.removeChild(boxTopping);
+    });
+
+});
+
