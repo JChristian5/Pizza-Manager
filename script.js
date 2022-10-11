694b796bf505d10a901d800b8603e9c9356c37bc
Jeremy Christian <jeremy.l.christian@gmail.com>
2022-10-11 16:23:32 -0700

    Change a few if statements to do while loops so it continues to prompt if user enters invalid response.

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ submitToppings.addEventListener('click', () => {
     const boxTopping = document.createElement('div');
     let name = toppingsName.value;
     let number = toppingsNumber.value;
-    if(toppingsName.value === '' || toppingsName.value >= 0 || toppingsName.value <= 0 || toppingsNumber.value < 1) {
-        alert('Error! Make sure you entered a valid name for the toppings or entered a number larger than 0.');
+    // Add character check for toppingsName so no numbers can be added to the name
+    if(toppingsName.value === '' || toppingsNumber.value < 1) {
+        alert('Error! Make sure you entered a valid name for the toppings (toppings can not contain numbers) or entered a number larger than 0.');
         return;
     }
     boxTopping.innerText = `${name}: ${number}`;
@@ -41,13 +42,13 @@ submitToppings.addEventListener('click', () => {
 
     editButton.addEventListener ('click', () => {
         let changedName = prompt('What would you like to rename your topping to?');
-        if(changedName === '' || changedName >= 0 || changedName <= 0) {
-            let changedName = prompt('Try again, name cannot be blank or a number');
+        while(changedName === '' || changedName >= 0 || changedName <= 0) {
+            changedName = prompt('Try again, name cannot be blank or a number');
         }
         let changedNumber = prompt('How many of that topping do you have?');
-        // if(changedNumber < 1) {
-        //     let changedNumber = prompt ('Try again, please enter a valid number');
-        // } Fix tomorrow
+        while(changedNumber < 1 || /^[a-zA-Z]+$/.test(changedNumber) === true || /^[0-9]+$/.test(changedNumber) === false) {
+             changedNumber = prompt ('Try again, please enter a valid number');
+        }
         boxTopping.innerText = `${changedName}: ${changedNumber}`;
         boxTopping.appendChild(editButton);
         boxTopping.appendChild(deleteButton);
