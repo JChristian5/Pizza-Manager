32e13e1f9d7468330c8f6b23322cc66989f6dc31
Jeremy Christian <jeremy.l.christian@gmail.com>
2022-10-12 21:28:06 -0700

    Use forEach to cycle through the different toppings that are selected on submit.

---
+++ script-pizzas.js
@@ -0,0 +1,24 @@
+// Pizza Making
+// Add temp ingredients for now
+
+const pizzasForm = document.querySelector('#pizzasform');
+const pepperoni = document.querySelector('#pepperoni');
+const ham = document.querySelector('#ham');
+const bacon = document.querySelector('#bacon');
+const jalapenos = document.querySelector('#jalapenos')
+const pineapple = document.querySelector('#pineapple');
+
+const submitPizza = document.querySelector('#submitpizza')
+
+pizzasForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+let ingredientArray = [pepperoni, ham, bacon, jalapenos, pineapple];
+
+submitPizza.addEventListener('click', () => {
+    ingredientArray.forEach((ingredient) => {
+        if(ingredient.checked === true) {
+            console.log(ingredient.name);
+        }
+    })
+})
