efc859bc1f9b08524a17a5a396edbadb403fe7e4
Jeremy Christian <jeremy.l.christian@gmail.com>
2022-10-14 13:27:58 -0700

    Remove original lines of code that we're going to be used for pizza making page.

--- script.js
+++ script.js
@@ -65,27 +65,3 @@ submitToppings.addEventListener('click', () => {
         boxTopping.appendChild(deleteButton);
     });
 });
-
-// Pizza Making
-// Add temp ingredients for now
-
-const pizzasForm = document.querySelector('#pizzasform');
-const pepperoni = document.querySelector('#pepperoni');
-const ham = document.querySelector('#ham');
-const bacon = document.querySelector('#bacon');
-const jalapenos = document.querySelector('#jalapenos')
-const pineapple = document.querySelector('#pineapple');
-
-const submitPizza = document.querySelector('#submitpizza')
-
-pizzasForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-});
-
-submitPizza.addEventListener('click', () => {
-    if(pizzasForm.checked === true) {
-        console.log('true');
-    } else {
-        console.log('false');
-    }
-})
