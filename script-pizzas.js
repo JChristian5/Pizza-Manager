3595894c35fed1a2f22f9cefcd4d4a6cd9cb15b8
Jeremy Christian <jeremy.l.christian@gmail.com>
2022-10-13 20:52:00 -0700

    Add innerText attribute to li items that updates the values as more pizzas are created.

--- script-pizzas.js
+++ script-pizzas.js
@@ -5,8 +5,21 @@ const pizzasForm = document.querySelector('#pizzasform');
 const pepperoni = document.querySelector('#pepperoni');
 const ham = document.querySelector('#ham');
 const bacon = document.querySelector('#bacon');
+    
 const jalapenos = document.querySelector('#jalapenos')
 const pineapple = document.querySelector('#pineapple');
+
+const pepperoniName = document.querySelector('#pepperoniname');
+const hamName = document.querySelector('#hamname');
+const baconName = document.querySelector('#baconname');
+const jalapenosName = document.querySelector('#jalapenosname');
+const pineappleName = document.querySelector('#pineapplename');
+
+pepperoniName.innerText = `Pepperoni: ${pepperoni.value}`;
+hamName.innerText = `Ham: ${ham.value}`;
+baconName.innerText = `Bacon: ${bacon.value}`;
+jalapenosName.innerText = `Jalapenos: ${jalapenos.value}`;
+pineappleName.innerText = `Pineapple: ${pineapple.value}`;
 
 const submitPizza = document.querySelector('#submitpizza')
 
@@ -23,11 +36,18 @@ submitPizza.addEventListener('click', () => {
         if(ingredient.checked === true) {
             console.log(ingredient.name);
             ingredient.value -= 1;
-
+            
             const toppingsListBox = document.createElement('div');
             toppingsListBox.classList.add('mb-2');
-            toppingsListBox.innerText = `${ingredient.name}`;
+            toppingsListBox.classList.add('border');
+            toppingsListBox.innerText = `Pizza ${ingredient.name}`;
             pizzaList.appendChild(toppingsListBox);
+
+            pepperoniName.innerText = `Pepperoni: ${pepperoni.value}`;
+            hamName.innerText = `Ham: ${ham.value}`;
+            baconName.innerText = `Bacon: ${bacon.value}`;
+            jalapenosName.innerText = `Jalapenos: ${jalapenos.value}`;
+            pineappleName.innerText = `Pineapple: ${pineapple.value}`;
         }
     });
 });
