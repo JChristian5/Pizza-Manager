b73914f85da285307809fcd30f5ac9602a9b015d
Jeremy Christian <jeremy.l.christian@gmail.com>
2026-05-16 10:37:48 -0700

    Added functionality for localStorage Created new funciton for displaying toppings Removed old commented code

--- script.js
+++ script.js
@@ -87,21 +87,11 @@ toppingsForm.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
-// let toppingsListArray = [];
-
 submitToppings.addEventListener('click', () => {
     
 
     let name = toppingsName.value;
     let number = toppingsNumber.value;
-
-    // let toppingExists = toppingsListArray.includes(toppingsName.value);
-    // if(toppingExists == true) {
-    //     alert('Error! Topping already exists!');
-    //     return;
-    // }
-
-    // toppingsListArray.push(name);
 
     if(
         name === '' || 
@@ -136,37 +126,4 @@ submitToppings.addEventListener('click', () => {
 
     toppingsName.value = '';
     toppings.value = '';
-
-    // boxTopping.innerText = `${name}: ${number}`;
-    // submitList.appendChild(boxTopping);
-    // toppingsName.value = ''; //Deletes text inside input box
-    // toppingsNumber.value = ''; //Deletes text inside input box
-
-    // const editButton = document.createElement('button');
-    // editButton.classList.add('btn', 'btn-outline-primary', 'ms-2', 'me-2');
-    // editButton.innerText = 'Edit';
-    // boxTopping.appendChild(editButton);
-    
-    // const deleteButton = document.createElement('button');
-    // deleteButton.classList.add('btn', 'btn-outline-danger', 'ms-2');
-    // deleteButton.innerText = 'Delete';
-    // boxTopping.appendChild(deleteButton); 
-
-    // deleteButton.addEventListener('click', () => {
-    //     submitList.removeChild(boxTopping);
-    // });
-
-    // editButton.addEventListener ('click', () => {
-    //     let changedName = prompt('What would you like to rename your topping to?');
-    //     while(changedName === '' || changedName >= 0 || changedName <= 0 || /^[a-zA-Z]*$/.test(changedName) === false) {
-    //         changedName = prompt('Try again, name cannot be blank or a number');
-    //     }
-    //     let changedNumber = prompt('How many of that topping do you have?');
-    //     while(changedNumber < 1 || /^[a-zA-Z]+$/.test(changedNumber) === true || /^[0-9]+$/.test(changedNumber) === false) {
-    //          changedNumber = prompt ('Try again, please enter a valid number');
-    //     }
-    //     boxTopping.innerText = `${changedName}: ${changedNumber}`;
-    //     boxTopping.appendChild(editButton);
-    //     boxTopping.appendChild(deleteButton);
-    // });
 });
